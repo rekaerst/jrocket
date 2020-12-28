0bf460ca2390fbfd18ca483d2a01e7fe2d128b54
Rekaerst <rekaerst@outlook.com>
2020-12-29 00:13:43 +0800

    [Window] Fix window size

--- src/com/rekaerst/Window.java
+++ src/com/rekaerst/Window.java
@@ -1,6 +1,7 @@
 package com.rekaerst;
 
 import java.awt.Canvas;
+import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -14,15 +15,18 @@ public class Window extends Canvas {
 
     public Window(int width, int height, String title, App app) {
         JFrame frame = new JFrame(title);
-        frame.setPreferredSize(new Dimension(width, height));
-        frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize(new Dimension(width, height));
+        Container panel = frame.getContentPane();
 
+        panel.setPreferredSize(new Dimension(width, height));
+        panel.setMaximumSize(new Dimension(width, height));
+        panel.setMinimumSize(new Dimension(width, height));
+        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.add(app);
         frame.setVisible(true);
         app.start();
+        System.out.println(panel.getSize());
     }
 }
